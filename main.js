5d95125cc63c425ae0ab38e2e44e8c096c3b3546
Yossef <yamar8@gmail.com>
2022-04-07 12:02:16 +0300

    add gitignore. progress in code

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-const cards = ['A','B','C','A','B','C'];
+const cards = ["A", "B", "C", "A", "B", "C"];
 
-// const element = document.createElement("div"); // create new element
-// element.innerHTML = "Hello";
-// console.log(element);
-// const board = document.getElementById("board");
-// board.appendChild(element);
-
-// const cards = ["A", "B", "C", "A", "B", "C"];
-// const board = document.getElementById("board");
-// for (i of cards) {
-//   let element = document.createElement("div");
-//   element.innerHTML = i;
-//   board.appendChild(element);
-// }
-
-
-function createCard(idx){
-    const cardEl = document.createElement("div");
-    cardEl.innerHTML = cards[idx];
-    cardEl.id = idx;
-    cardEl.className = "card";
-    return cardEl;
+function createCard(idx) {
+  const cardEl = document.createElement("div");
+  cardEl.innerHTML = cards[idx];
+  cardEl.id = idx;
+  cardEl.className = "card";
+  return cardEl;
 }
 
+function shuffle(array) {  //from stackoverflow
+  let currentIndex = array.length, randomIndex;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = array[currentIndex];
+  }
+  return array;
+}
 
-function init(){
-    //shuffle(cards);
-    //append cards to board
-    const board = document.getElementById("board");
-    for(i in cards){
-        const element = createCard(i);
-        board.appendChild(element);
-    }
+function init() {
+  shuffle(cards);
+  //append cards to board
+  const board = document.getElementById("board");
+  for (i in cards) {
+    const element = createCard(i);
+    board.appendChild(element);
+  }
 }
 
 init();
